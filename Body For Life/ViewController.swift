c708e3b910acf620e3cdcf832efb15e2c6a48db6
Aramis Knox <carbon.moknoxide@gmail.com>
2019-06-21 18:48:05 -0700

    Initial Commit

---
+++ Body For Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Body For Life
+//
+//  Created by Aramis Knox on 6/21/19.
+//  Copyright © 2019 Aramis Knox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
